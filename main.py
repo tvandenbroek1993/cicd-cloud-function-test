ce4de4ea06b08f8a3c363c89166c3c6bb8f865d1
tvandenbroek1993 <tom.vandenbroek@springbokagency.com>
2023-10-04 15:45:38 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main(request):
+    """
+    Args:
+        request (flask.Request): The request object
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+    """
+    return "Hoi World!"
